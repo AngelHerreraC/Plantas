444b1a88a921081c8bcb520abed0d7bf9b915136
Adalbert047 <aadalberto835@gmail.com>
2023-12-03 13:42:22 -0600

    Preparacion

---
+++ Sources/scripts/register.js
@@ -0,0 +1,10 @@
+// const form = document.querySelector('#form');
+
+// const procesaTodo = (event) => 
+// {
+//     event.preventDefault();
+//     const datos = new FormData()
+
+//     const datosCompletos = Object.fromEntries(datos.entries())
+//     console.log(da)
+// }
