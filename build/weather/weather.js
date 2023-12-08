3c27555eca98bd37950e56d45bf981bf8abfe5e7
Adalbert047 <aadalberto835@gmail.com>
2023-12-08 17:20:54 -0600

    Avances nomas desde lap

---
+++ build/weather/weather.js
@@ -0,0 +1,16 @@
+document.querySelector("#btn").addEventListener('click', e => 
+{
+
+    const clima = document.querySelector("#tipo-clima-usuario")
+
+
+    console.log(clima.value)
+
+    alert("Se registro el tipo de clima")
+})
+
+
+function guardarLocalStorage()
+{
+    
+}
